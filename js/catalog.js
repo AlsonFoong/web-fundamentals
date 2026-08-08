462beb523f082483696c87aa8abfeb96311c12a4
Alson <alsonfoong2007@gmail.com>
2026-08-08 16:11:34 +0800

    Implement catalog filter and polish content

--- js/catalog.js
+++ js/catalog.js
@@ -1,21 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const searchForm = document.querySelector('.catalog-form')
-    const searchBar = document.getElementsByClassName('catalog-search-container')
-    const catalogItems = document.querySelectorAll('.catalog-item-container > li')
+    const searchForm = document.getElementById('catalog-form')
+    const searchContainer = document.getElementById('catalog-search-container')
+    const searchBar = document.getElementById('catalog-search')
+    const searchFilter = document.getElementById('search-filter')
+    const searchFilterResetButton = document.getElementById('search-filter-reset')
+    const searchFilterDropdown = document.getElementById('dropdown-menu')
+    const catalogItems = document.querySelectorAll('#catalog-item-container > li')
+
+    let shown = false
+    let query = ''
+    let filters = []
+
+    searchFilter.addEventListener('click', (e) => {
+        if (!shown) {
+            searchFilterDropdown.classList.add('visible')
+            searchFilter.setAttribute('aria-expanded', 'true')
+        } else {
+            searchFilterDropdown.classList.remove('visible')
+            searchFilter.setAttribute('aria-expanded', 'false')
+        }
+        shown = !shown
+    })
 
     searchForm.addEventListener('submit', (e) => {
         e.preventDefault()
     })
 
-    searchBar[0].addEventListener('input', (e) => {
-        const query = e.target.value.toLowerCase().trim()
-
+    function applyFilters() {
         catalogItems.forEach(item => {
             const itemText = item.textContent.toLowerCase()
             const itemTags = item.dataset.tags ? item.dataset.tags.toLowerCase() : ''
 
-            const isMatch = itemText.includes(query) || itemTags.includes(query)
-            item.style.display = isMatch ? '' : 'none'
+            const matchesQuery = !query || itemText.includes(query) || itemTags.includes(query)
+            const matchesFilters = filters.every(filter => itemTags.includes(filter))
+
+            item.style.display = (matchesQuery && matchesFilters) ? '' : 'none'
         })
+    }
+
+    searchContainer.addEventListener('input', (e) => {
+        if (e.target === searchBar) {
+            query = searchBar.value.toLowerCase().trim()
+        } else {
+            // Must be a checkbox
+            const value = e.target.value.toLowerCase().trim()
+            if (e.target.checked) {
+                filters.push(value)
+            } else {
+                filters.splice(filters.indexOf(value), 1)
+            }
+        }
+        applyFilters()
+    })
+
+    searchFilterResetButton.addEventListener('click', (e) => {
+        query = ''
+        filters = []
+        applyFilters()
     })
 })
